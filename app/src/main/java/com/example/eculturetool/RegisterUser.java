ef9524703f61ec6669371edcfd64dfa04106831b
Lorenzo <lorenzo.gml@gmail.com>
2022-03-03 19:47:47 +0100

    Riorganizzazione del codice

--- app/src/main/java/com/example/eculturetool/RegisterUser.java
+++ app/src/main/java/com/example/eculturetool/RegisterUser.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +17,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
 
@@ -26,7 +24,6 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
     private EditText editTextNome, editTextCognome, editTextEmail, editTextPassword;
     private TextView registerUser;
     private final int PASSWORD_LENGTH = 6;
-    boolean risultato = false;
 
 
     @Override
