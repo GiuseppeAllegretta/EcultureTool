ceb77fd7dee34ba0f734cbb677aa3711ed596082
MatteoTT <matteo.attanasio98@gmail.com>
2022-03-04 13:08:32 +0100

    Inserimento barra di navigazione, modifica layout registrazione

---
+++ app/src/main/java/com/example/eculturetool/Home.java
@@ -0,0 +1,52 @@
+package com.example.eculturetool;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.eculturetool.databinding.ActivityMainBinding;
+import com.example.eculturetool.databinding.HomeBinding;
+import com.example.eculturetool.fragments.HomeFragment;
+import com.example.eculturetool.fragments.PlacesFragment;
+import com.example.eculturetool.fragments.QRScannerFragment;
+
+public class Home extends AppCompatActivity {
+
+    HomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = HomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.places:
+                    replaceFragment(new PlacesFragment());
+                    break;
+                case R.id.qr_scanner:
+                    replaceFragment(new QRScannerFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
