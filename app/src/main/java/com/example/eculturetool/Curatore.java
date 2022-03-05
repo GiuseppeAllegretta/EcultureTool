de0da5b41a320f8c51a8e14ca5cf23b5dc521f9e
Giovanni <gio.napoli13@gmail.com>
2022-03-05 18:06:25 +0100

    Commit registrazione su realtime database

--- app/src/main/java/com/example/eculturetool/Curatore.java
+++ app/src/main/java/com/example/eculturetool/Curatore.java
@@ -2,15 +2,29 @@ package com.example.eculturetool;
 
 public class Curatore {
 
-     public String nome, cognome, email;
+     public String uid, nome, cognome, email;
 
     public Curatore() {
     }
 
-    public Curatore(String nome, String cognome, String email) {
+    public String getUid(){
+        return uid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public Curatore(String uid, String nome, String cognome, String email) {
+        this.uid = uid;
         this.nome = nome;
         this.cognome = cognome;
         this.email = email;
+
     }
 
     public void setNome(String nome) {
--- app/src/main/java/com/example/eculturetool/fragments/HomeFragment.java
+++ app/src/main/java/com/example/eculturetool/fragments/HomeFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.eculturetool.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
