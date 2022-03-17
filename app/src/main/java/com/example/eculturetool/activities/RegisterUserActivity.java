e667186d2570607b30f9d11b2cb1d5c2567f6a23
domenicobrigido <d.brigido2@studenti.uniba.it>
2022-03-17 19:33:01 +0100

    Riordinato codice e cancellati inutili import e attributi

--- app/src/main/java/com/example/eculturetool/activities/RegisterUserActivity.java
+++ app/src/main/java/com/example/eculturetool/activities/RegisterUserActivity.java
@@ -5,24 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.eculturetool.R;
 import com.example.eculturetool.entities.Curatore;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegisterUserActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,9 +23,7 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
     private EditText editTextNome, editTextCognome, editTextEmail, editTextPassword;
     private TextView registerUser;
     private final int PASSWORD_LENGTH = 6;
-    private static final String TAG = "EmailPassword";
     private ProgressBar progressBar;
-    String nomeRestored = null;
 
 
     @Override
@@ -132,6 +123,7 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
         CharSequence charSequence = editTextNomeSavedState.getText();
         savedInstanceState.putCharSequence("MySavedData", charSequence);
     }
+
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
