abaaa7401eab4a0f082cdf2f7059d6a04c424788
domenicobrigido <d.brigido2@studenti.uniba.it>
2022-03-22 00:04:42 +0100

    Sistemati alcuni problemi (recycle view oggetti non viene visualizzata sulla schermata)

--- app/src/main/java/com/example/eculturetool/RecyclerAdapterOggetto.java
+++ app/src/main/java/com/example/eculturetool/RecyclerAdapterOggetto.java
@@ -4,9 +4,7 @@ package com.example.eculturetool;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
-import android.widget.ImageView;
+
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.eculturetool.entities.Oggetto;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
