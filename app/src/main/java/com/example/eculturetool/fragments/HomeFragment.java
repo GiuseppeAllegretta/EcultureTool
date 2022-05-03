287da0cbc385f0195099ed8c36c0264de7a45d23
domenicobrigido <d.brigido2@studenti.uniba.it>
2022-05-03 18:46:25 +0200

    Implementata guida per l'utente nella sezione Home

--- app/src/main/java/com/example/eculturetool/fragments/HomeFragment.java
+++ app/src/main/java/com/example/eculturetool/fragments/HomeFragment.java
@@ -2,11 +2,14 @@ package com.example.eculturetool.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,12 +21,17 @@ import com.example.eculturetool.R;
 import com.example.eculturetool.activities.CreazionePercorsoActivity;
 import com.example.eculturetool.activities.LuoghiActivity;
 import com.example.eculturetool.activities.PercorsiActivity;
+import com.example.eculturetool.activities.SplashActivity;
 import com.example.eculturetool.activities.ZoneActivity;
 import com.example.eculturetool.activities.OggettiActivity;
 import com.example.eculturetool.database.Connection;
 import com.example.eculturetool.database.DataBaseHelper;
+import com.example.eculturetool.database.SessionManagement;
 import com.example.eculturetool.entities.Curatore;
 import com.example.eculturetool.entities.Luogo;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
+import com.getkeepsafe.taptargetview.TapTargetView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,6 +43,7 @@ public class HomeFragment extends Fragment {
     private TextView tv, luogoGestito;
     private CardView percorsi, luoghi, zone, oggetti;
     private Context context;
+    private ImageView showTutorial;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -84,6 +93,8 @@ public class HomeFragment extends Fragment {
         zone = view.findViewById(R.id.zoneCard);
         oggetti = view.findViewById(R.id.oggettiCard);
         luogoGestito = view.findViewById(R.id.nomeLuogoHome);
+        showTutorial=view.findViewById(R.id.showTutorialHome);
+
         dataBaseHelper = new DataBaseHelper(getActivity().getApplicationContext());
         dataBaseHelper.getCuratore();
 
@@ -99,6 +110,18 @@ public class HomeFragment extends Fragment {
         if(luogo != null){
             luogoGestito.setText(Html.fromHtml(context.getString(R.string.stai_gestendo) + " " + "<b>" + luogo.getNome() + "</b>", 0));
         }
+
+
+
+
+
+        showTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTutorial();
+            }
+        });
+
 
         percorsi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -138,6 +161,8 @@ public class HomeFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -155,4 +180,98 @@ public class HomeFragment extends Fragment {
             luogoGestito.setText(Html.fromHtml(context.getString(R.string.stai_gestendo) + " " + "<b>" + luogo.getNome() + "</b>", 0));
         }
     }
+
+
+
+    private void showTutorial(){
+        new TapTargetSequence(getActivity()).targets(
+                TapTarget.forView(luoghi,"Pulsante Luoghi","Potrai gestire da qui\n i tuoi luoghi")
+                        // All options below are optional
+                        .outerCircleColor(R.color.gialloSecondario)
+                        .outerCircleAlpha(0.96f)
+                        .targetCircleColor(R.color.white)
+                        .titleTextSize(15)
+                        .titleTextColor(R.color.white)
+                        .descriptionTextSize(12)
+                        .descriptionTextColor(R.color.white)
+                        .textColor(R.color.black)
+                        .textTypeface(Typeface.SANS_SERIF)
+                        .dimColor(R.color.black)
+                        .drawShadow(true)
+                        .cancelable(false)
+                        .tintTarget(false)
+                        .transparentTarget(true)
+                        .targetRadius(60),
+
+                TapTarget.forView(zone,"Pulsante Zone","Potrai gestire da qui\n le tue zone")
+                        // All options below are optional
+                        .outerCircleColor(R.color.gialloSecondario)
+                        .outerCircleAlpha(0.96f)
+                        .targetCircleColor(R.color.white)
+                        .titleTextSize(15)
+                        .titleTextColor(R.color.white)
+                        .descriptionTextSize(12)
+                        .descriptionTextColor(R.color.white)
+                        .textColor(R.color.black)
+                        .textTypeface(Typeface.SANS_SERIF)
+                        .dimColor(R.color.black)
+                        .drawShadow(true)
+                        .cancelable(false)
+                        .tintTarget(false)
+                        .transparentTarget(true)
+                        .targetRadius(60),
+
+                TapTarget.forView(oggetti,"Pulsante Oggetti","Potrai gestire da qui\n i tuoi oggetti")
+                        // All options below are optional
+                        .outerCircleColor(R.color.gialloSecondario)
+                        .outerCircleAlpha(0.96f)
+                        .targetCircleColor(R.color.white)
+                        .titleTextSize(15)
+                        .titleTextColor(R.color.white)
+                        .descriptionTextSize(12)
+                        .descriptionTextColor(R.color.white)
+                        .textColor(R.color.black)
+                        .textTypeface(Typeface.SANS_SERIF)
+                        .dimColor(R.color.black)
+                        .drawShadow(true)
+                        .cancelable(false)
+                        .tintTarget(false)
+                        .transparentTarget(true)
+                        .targetRadius(60),
+                TapTarget.forView(percorsi,"Pulsante Percorsi","Potrai creare e gestire \n i tuoi percorsi da qui")
+                        // All options below are optional
+                        .outerCircleColor(R.color.gialloSecondario)
+                        .outerCircleAlpha(0.96f)
+                        .targetCircleColor(R.color.white)
+                        .titleTextSize(15)
+                        .titleTextColor(R.color.white)
+                        .descriptionTextSize(12)
+                        .descriptionTextColor(R.color.white)
+                        .textColor(R.color.black)
+                        .textTypeface(Typeface.SANS_SERIF)
+                        .dimColor(R.color.black)
+                        .drawShadow(true)
+                        .cancelable(false)
+                        .tintTarget(false)
+                        .transparentTarget(true)
+                        .targetRadius(60))
+                .listener(new TapTargetSequence.Listener() {
+                    @Override
+                    public void onSequenceFinish() {
+
+                    }
+
+                    @Override
+                    public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+
+                    }
+
+                    @Override
+                    public void onSequenceCanceled(TapTarget lastTarget) {
+
+                    }
+                }).start();
+
+    }
+
 }
