35c51c4a6e086de8f915803264f9d7fcd753c9ae
domenicobrigido <d.brigido2@studenti.uniba.it>
2022-05-03 18:47:47 +0200

    pulizia codice

--- app/src/main/java/com/example/eculturetool/fragments/QRcodeScannerFragment.java
+++ app/src/main/java/com/example/eculturetool/fragments/QRcodeScannerFragment.java
@@ -1,12 +1,9 @@
 package com.example.eculturetool.fragments;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+
 import android.graphics.Typeface;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,23 +12,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.eculturetool.R;
-import com.example.eculturetool.activities.DettaglioOggettoActivity;
-import com.example.eculturetool.database.DataBaseHelper;
-import com.example.eculturetool.entities.Oggetto;
-import com.example.eculturetool.entities.Zona;
+
 import com.example.eculturetool.utilities.Capture;
 import com.getkeepsafe.taptargetview.TapTarget;
-import com.getkeepsafe.taptargetview.TapTargetSequence;
+
 import com.getkeepsafe.taptargetview.TapTargetView;
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
-import java.io.Serializable;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
