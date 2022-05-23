eff3adf1785b47a68be4fad284cef4bb8ccd4101
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-05-23 18:58:30 +0200

    Implementata la funzionalità di visualizzazione icona del cerca nella ricerca generale

--- app/src/main/java/com/example/eculturetool/fragments/SearchFragment.java
+++ app/src/main/java/com/example/eculturetool/fragments/SearchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -88,7 +89,13 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbarEntita);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+
+        return view;
     }
 
     @Override
