10e5e724a652e222660c2135744779e50b243265
Giovanni <gio.napoli13@gmail.com>
2022-05-23 18:40:38 +0200

    implementazione ricerca base gloale2

--- app/src/main/java/com/example/eculturetool/fragments/SearchFragment.java
+++ app/src/main/java/com/example/eculturetool/fragments/SearchFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,12 +26,14 @@ import com.example.eculturetool.entities.Tipologia;
 import com.example.eculturetool.utility_percorsi.RecyclerAdapterEntita;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class SearchFragment extends Fragment {
 
     private ArrayList<Entita> entitaList;
     private RecyclerView recyclerView;
+    private RecyclerAdapterEntita adapterEntita;
 
 
     public SearchFragment() {
@@ -44,12 +51,10 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     private void setAdapter() {
-        RecyclerAdapterEntita adapterEntita= new RecyclerAdapterEntita(entitaList);
+        adapterEntita= new RecyclerAdapterEntita(entitaList);
         RecyclerView.LayoutManager layoutManager;
         layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -81,7 +86,30 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu,menu);
+        MenuItem item = menu.findItem(R.id.ricerca);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapterEntita.getFilter().filter(newText);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
 }
--- app/src/main/java/com/example/eculturetool/utility_percorsi/RecyclerAdapterEntita.java
+++ app/src/main/java/com/example/eculturetool/utility_percorsi/RecyclerAdapterEntita.java
@@ -3,6 +3,8 @@ package com.example.eculturetool.utility_percorsi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,14 +14,49 @@ import com.example.eculturetool.R;
 import com.example.eculturetool.entities.Entita;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class RecyclerAdapterEntita extends RecyclerView.Adapter<RecyclerAdapterEntita.EntitaViewHolder> {
+public class RecyclerAdapterEntita extends RecyclerView.Adapter<RecyclerAdapterEntita.EntitaViewHolder> implements Filterable {
     private ArrayList<Entita> entitaList;
+    private ArrayList<Entita> entitaListAll;
 
     public RecyclerAdapterEntita(ArrayList<Entita> entitaList) {
         this.entitaList=entitaList;
+        this.entitaListAll = new ArrayList<>(entitaList);
 
     }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+    Filter filter = new Filter() {
+        //eseguito in background
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<Entita> filteredList = new ArrayList<>();
+            if(constraint.toString().isEmpty()){
+                filteredList.addAll(entitaListAll);
+            }else{
+                for(Entita e : entitaListAll){
+                    if (e.getNome().toLowerCase().contains(constraint.toString().toLowerCase())){
+                        filteredList.add(e);
+                    }
+                }
+
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values=filteredList;
+            return filterResults;
+        }
+        //eseguito nell'ui
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            entitaList.clear();
+            entitaList.addAll((Collection<? extends Entita>) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class EntitaViewHolder extends RecyclerView.ViewHolder{
         private TextView nomeEntita;
