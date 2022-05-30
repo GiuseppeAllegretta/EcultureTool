e6d715d60472809440e5efb8c02a9acb920f654a
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-05-30 19:10:04 +0200

    Pulizia del codice e commenti

--- app/src/main/java/com/example/eculturetool/utilities/RecyclerAdapterPercorso.java
+++ app/src/main/java/com/example/eculturetool/utilities/RecyclerAdapterPercorso.java
@@ -6,7 +6,6 @@ import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
