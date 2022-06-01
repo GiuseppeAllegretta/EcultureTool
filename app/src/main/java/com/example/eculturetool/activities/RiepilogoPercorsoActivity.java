cb37bdeeb1cebb84c35ad40575f5a390d13ef030
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-06-01 18:55:03 +0200

    Implementato spinner per la visualizzazione degli oggetti contenuti in ogni zona

--- app/src/main/java/com/example/eculturetool/activities/RiepilogoPercorsoActivity.java
+++ app/src/main/java/com/example/eculturetool/activities/RiepilogoPercorsoActivity.java
@@ -1,5 +1,6 @@
 package com.example.eculturetool.activities;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -9,7 +10,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +26,17 @@ import com.example.eculturetool.R;
 import com.example.eculturetool.database.DataBaseHelper;
 import com.example.eculturetool.database.IoHelper;
 import com.example.eculturetool.entities.DataHolder;
+import com.example.eculturetool.entities.Oggetto;
+import com.example.eculturetool.entities.TipologiaOggetto;
 import com.example.eculturetool.entities.Zona;
 import com.example.eculturetool.view.GraphView;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RiepilogoPercorsoActivity extends AppCompatActivity {
     private Button eliminaBtn;
@@ -37,6 +46,12 @@ public class RiepilogoPercorsoActivity extends AppCompatActivity {
     private IoHelper ioHelper;
     private int idPercorso;
     private DataBaseHelper dataBaseHelper;
+    private TextInputLayout spinner;
+    private AutoCompleteTextView autoCompleteTextView;
+
+    private List<String> zoneNomi = new ArrayList<>();
+    private List<Oggetto> oggetti = new ArrayList<>();
+    private List<Zona> zone;
 
     //TODO in tasto condividi genera eccezione
 
@@ -75,12 +90,75 @@ public class RiepilogoPercorsoActivity extends AppCompatActivity {
 
         Zoomy.Builder builder = new Zoomy.Builder(this).target(graphView);
         builder.register();
+
+        gestioneSpinner();
+    }
+
+    private void gestioneSpinner() {
+        settaggioSpinner();
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                for(Zona zona: zone){
+                    if(zona.getNome().compareTo(zoneNomi.get(i)) == 0){
+                        oggetti.addAll(zona.getListaOggetti());
+                        break;
+                    }
+
+                    for(Zona zonaDiramazione: zona.getDiramazione()){
+                        if(zonaDiramazione.getNome().compareTo(zoneNomi.get(i)) == 0){
+                            oggetti.addAll(zonaDiramazione.getListaOggetti());
+                            break;
+                        }
+                    }
+                }
+
+                String nomiOggetti = "\n";
+
+
+                for(Oggetto oggetto: oggetti){
+                    nomiOggetti = nomiOggetti + oggetto.getNome() + " \n\n";
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(RiepilogoPercorsoActivity.this);
+                builder.create();
+                builder.setTitle("Oggetti nella zona " + "\"" + zoneNomi.get(i) + "\"").setMessage(nomiOggetti).show();
+                oggetti.clear();
+            }
+        });
+    }
+
+    private void settaggioSpinner() {
+        spinner = findViewById(R.id.materialSpinner);
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+
+        zone = ioHelper.listZoneDeserializzazione(idPercorso);
+        zoneNomi = new ArrayList<>();
+        oggetti = new ArrayList<>();
+
+        for(Zona zona: zone){
+            if(!zoneNomi.contains(zona.getNome())){
+                zoneNomi.add(zona.getNome());
+            }
+
+            for(Zona zonaDiramazione: zona.getDiramazione()){
+                if(!zoneNomi.contains(zonaDiramazione.getNome())){
+                    zoneNomi.add(zonaDiramazione.getNome());
+                }
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, zoneNomi);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        autoCompleteTextView.setAdapter(adapter);
     }
 
     private void scritturaSuFileJson() {
         //Appena ottenuti i dati del grafo vengno salvati tra i file
         ioHelper.esportaTxtinJsonFormat(graph, idPercorso);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
