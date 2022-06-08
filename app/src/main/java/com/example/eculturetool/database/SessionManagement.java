81d6566de6443860a363b5c6bf75f61cf669ce31
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-06-08 08:50:58 +0200

    commentato il codice relativo alla classe della sessione

--- app/src/main/java/com/example/eculturetool/database/SessionManagement.java
+++ app/src/main/java/com/example/eculturetool/database/SessionManagement.java
@@ -3,6 +3,9 @@ package com.example.eculturetool.database;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * Classe che si occupa di gestire la sessione dell'utente
+ */
 public class SessionManagement {
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -14,14 +17,25 @@ public class SessionManagement {
         editor = sharedPreferences.edit();
     }
 
+    /**
+     * Metodo che si occupa di salvare la sessione
+     * @param emailCuratore da salvare in modo tale da recuperare la sessione
+     */
     public void saveSession(String emailCuratore) {
         editor.putString(SESSION_KEY, emailCuratore).commit();
     }
 
+    /**
+     * Metoto che prende i riferimento della sessione e li restituisce
+     * @return riferimenti della sessione
+     */
     public String getSession() {
         return sharedPreferences.getString(SESSION_KEY, "-1");
     }
 
+    /**
+     * Metodo che rimuove la sessione
+     */
     public void removeSession() {
         editor.putString(SESSION_KEY, "-1").commit();
     }
