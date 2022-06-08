5011774875a3fda38fc27500ea4f3ae9e2be1627
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-06-08 11:01:00 +0200

    Aggiustata string ain inglese e commentata Capture.java

--- app/src/main/java/com/example/eculturetool/utilities/Capture.java
+++ app/src/main/java/com/example/eculturetool/utilities/Capture.java
@@ -2,5 +2,8 @@ package com.example.eculturetool.utilities;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * Classe che identifica la cattura del QR code
+ */
 public class Capture extends CaptureActivity {
 }
